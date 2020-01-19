908584e2e961fdd4fe173ed9a1404c07b5884e3c
OTABEK <aravanshop@gmail.com>
2020-01-19 14:20:27 +0600

    Исправка

---
+++ js/myfile.js
@@ -0,0 +1,7 @@
+// document.querySelector(".btn").addEventListener('click', function () {
+//     let box = document.querySelector(".box");
+//     let nav = document.querySelector(".box-nav");
+//     box.classList.toggle("box_widht");
+//     nav.classList.toggle("dsb");
+
+// });
