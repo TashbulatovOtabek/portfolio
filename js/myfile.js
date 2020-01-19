bf1b12d3e1d88465e6bc18426e4044f7d137ec98
OTABEK <aravanshop@gmail.com>
2020-01-19 20:58:48 +0600

    первый запись

--- js/myfile.js
+++ js/myfile.js
@@ -1,7 +1,18 @@
-// document.querySelector(".btn").addEventListener('click', function () {
-//     let box = document.querySelector(".box");
-//     let nav = document.querySelector(".box-nav");
-//     box.classList.toggle("box_widht");
-//     nav.classList.toggle("dsb");
-
+// let worldIcon = document.querySelector(".img__myFoto").addEventListener("click", function(){
+//     document.querySelector(".icon-react").classList.toggle("active__react");
 // });
+// document.querySelector(".btn").addEventListener("click", function () {
+//     document.querySelector(".web-container").classList.toggle("web-opas");
+// });
+
+
+
+document.querySelector(".web-container").addEventListener("click", function () {
+    document.querySelector(".html-container").classList.toggle("html-pos");
+    document.querySelector(".css-container").classList.toggle("css-pos");
+    document.querySelector(".js-container").classList.toggle("js-pos");
+    document.querySelector(".react-container").classList.toggle("react-pos");
+    document.querySelector(".reactNative-container").classList.toggle("reactNative-pos");
+    document.querySelector(".php-container").classList.toggle("php-pos");
+
+});
